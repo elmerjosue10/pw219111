f7cb4a2996ccb793efb8b9db612d956d99780ec5
elmerjosue10 <mendoza-elmer@hotmail.com>
2019-05-09 11:53:27 -0600

    vue - pendiente

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -2,6 +2,35 @@ const app = new Vue({
 	el: "#app",
 	data: {
 		titulo: 'Hola mundo con VUE',
-		frutas: ['manzanas','peras','platano','mango']
+		frutas: ['manzanas','peras','platano','mango'],
+		frutas2: [
+           {nombre:'manzana', cantidad: 10},
+           {nombre:'peras', cantidad: 0},
+           {nombre:'platano', cantidad: 20},
+           {nombre:'mango', cantidad: 80}
+
+		],
+		nuevaFruta: ''
+		total = 0
+	},
+	methods: {
+		agregarFruta(){
+			//console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			});
+			this.nuevaFruta = '';
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0;
+			for(fruta of this.frutas2){
+				this.total = this.total+fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 });
